c2108d4b3f81b7e468b62620d1f2685df6a7130d
SubarudoDuck <nghiavm2304@gmail.com>
2025-09-11 12:41:17 +1000

    [Chore] Deleted unwanted interfaces for better ones

--- src/server/interfaces/response.interface.ts
+++ src/server/interfaces/response.interface.ts
@@ -1,18 +1,5 @@
-// import type { Stop } from "./stop.interface";
-// import type { Departure } from "./departure.interface";
-
 export interface IResponse<T> {
   success: boolean;
   message?: string;
   data?: T
 };
-
-// export interface StopsResponse {
-//   stops: Stop[];
-//   totalCount: number
-// }
-
-// export interface DeparturesResponse {
-//   departures: Departure[];
-//   totalCount: number;
-// }
--- src/server/interfaces/route.interface.ts
+++
@@ -1,8 +0,0 @@
-// import type { Stop } from "./stop.interface";
-
-// export interface Route {
-//   route_id: number;
-//   route_name: string;
-//   route_type: number;
-//   stops: Stop[];
-// };
